3530a0324430ae0f9ec9bfe10507a991b2f6837b
Michael Teagle <mkteagle@gmail.com>
2015-12-07 16:38:36 -0700

    init commit.

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/**
+ * Created by i68066 on 12/7/15.
+ */
+function stageController($scope) {
+    $scope.model = 'author'; // contains the name of the variable that i would bind   to the field
+    $scope.vars = {};    // variables container
+    $scope.vars.realModel = 'Michael Teagle';
+}
